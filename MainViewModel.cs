7541770ca10bc8b5dd054695f2ab43df2f756ab9
jagansai <jagan.sai@outlook.com>
2023-11-30 17:52:12 +0800

    Adding first commit of TradeViewer

---
+++ MainViewModel.cs
@@ -0,0 +1,168 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Media;
+using System.Collections.ObjectModel;
+
+namespace TradeViewer
+{
+    public class TradeViewerConstants
+    {
+        public static readonly string Buy = "Buy";
+        public static readonly string Sell = "Sell";
+        public static readonly string SOLD = "Sold";
+        public static readonly string CAN_SELL = "Can Sell";
+        public static readonly string CANNOT_SELL = "Cannot Sell";
+    }
+
+    public class TradeData
+    {        
+        public string Date { get; set; }
+
+        public string Stock { get; set; }
+        public string Action { get; set; }
+    }
+
+    public class EnrichedTradeData : TradeData
+    {
+        public string HeldFor { get; set; }
+    }
+
+    public class StockToTradeGroup
+    {
+        private static readonly SolidColorBrush GREEN = new SolidColorBrush(Colors.Green);
+        private static readonly SolidColorBrush RED = new SolidColorBrush(Colors.Red);
+        public static readonly int HOLDING_PERIOD = 60;
+        private int _holding_Period = 0;
+
+        public string StockName { get; set; }
+        public int HeldFor
+        {
+            get; set;
+        }
+
+        public int HoldingPeriod
+        {
+            set
+            {
+                _holding_Period = value;
+            }
+            get
+            {
+                return _holding_Period > 0 ? _holding_Period : HOLDING_PERIOD;
+            }
+        }
+
+
+        public SolidColorBrush CanSell
+        {
+            get
+            {
+                if (CanSellDescr.Equals(TradeViewerConstants.SOLD) )
+                {
+                    return RED;
+                }
+
+                return HeldFor >= HoldingPeriod ? GREEN : RED;
+            }
+        }
+
+        public String CanSellDescr
+        {
+            get; set;
+        }
+
+        public IEnumerable<EnrichedTradeData> Trades { get; set; }
+
+        public static int calculateHeldFor( IEnumerable<TradeData> trades )
+        {
+            if (trades.Count() != 0)
+            {
+                return DateTimeOffset.Now.Subtract(DateTimeOffset.ParseExact(trades.First().Date, "dd-MMM-yyyy", CultureInfo.InvariantCulture)).Days;
+            }
+            return 0;
+        }
+
+        public static string calculateCanSell( IEnumerable<TradeData> trades, int heldForDays, int holding_period )
+        {
+            if (trades.Count() != 0)
+            {
+                return trades.First().Action.Equals(TradeViewerConstants.Sell) ? TradeViewerConstants.SOLD
+                                                                      : heldForDays >= holding_period ? TradeViewerConstants.CAN_SELL : TradeViewerConstants.CANNOT_SELL;
+            }
+            return heldForDays >= holding_period ? TradeViewerConstants.CAN_SELL : TradeViewerConstants.CANNOT_SELL;
+        }
+    }
+
+
+    public class MainViewModel
+    {
+        private ObservableCollection<StockToTradeGroup> records = new ObservableCollection<StockToTradeGroup>();
+
+        public ObservableCollection<StockToTradeGroup> Records
+        {
+            get
+            {
+                return records;
+            }
+        }
+
+        public ObservableCollection<StockToTradeGroup> LoadCsvData(string csvFilePath, int holding_period )
+        {
+            using (var reader = new StreamReader(csvFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+
+                var groupData = from trade in csv.GetRecords<TradeData>()
+                                group trade by trade.Stock into stockGroup
+                                select new { StockName = stockGroup.Key, Trades = EnrichTrades(stockGroup) };
+
+                foreach (var data in groupData)
+                {
+
+                    List<EnrichedTradeData> enrichedTradeDatas = new List<EnrichedTradeData>();
+
+                    EnrichedTradeData? temp = null;
+
+                    foreach (var trade in data.Trades)
+                    {
+                        if ( temp != null && temp.HeldFor == null )
+                        {
+                            var tempDate = DateTimeOffset.ParseExact(temp.Date, "dd-MMM-yyyy", CultureInfo.InvariantCulture);
+                            var tradeDate = DateTimeOffset.ParseExact(trade.Date, "dd-MMM-yyyy", CultureInfo.InvariantCulture);
+                            temp.HeldFor = tempDate.Subtract(tradeDate).Days.ToString();
+                        }
+                        var enrichedTrade = new EnrichedTradeData { Stock = trade.Stock, Action = trade.Action, Date = trade.Date };
+                        enrichedTradeDatas.Add( enrichedTrade);
+                        if ( trade.Action.Equals(TradeViewerConstants.Sell))
+                        {
+                            temp = enrichedTrade;
+                        }
+                    }
+                    int heldForDays = StockToTradeGroup.calculateHeldFor(enrichedTradeDatas);
+                    holding_period = holding_period != 0 ? holding_period : StockToTradeGroup.HOLDING_PERIOD;
+                    var stockToTradeGroup = new StockToTradeGroup()
+                    { StockName = data.StockName,
+                        Trades = enrichedTradeDatas,
+                        HeldFor = heldForDays,
+                        CanSellDescr = StockToTradeGroup.calculateCanSell(enrichedTradeDatas, heldForDays, holding_period),
+                        HoldingPeriod = holding_period
+                    };
+                    records.Add(stockToTradeGroup);
+                }
+            }
+            return records;
+        }
+
+        private List<TradeData> EnrichTrades(IGrouping<string, TradeData> stockGroup)
+        {
+            return stockGroup.OrderByDescending(x => DateTimeOffset.ParseExact(x.Date.Trim(), "dd-MMM-yyyy", CultureInfo.InvariantCulture))
+                             .ThenByDescending(x => x.Action).ToList();
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TradeViewer
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+
+    public partial class MainWindow : Window
+    {
+        private String fileName = String.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadCsvButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new Microsoft.Win32.OpenFileDialog() { Filter = "CSV Files|*.csv" };
+            var result = ofd.ShowDialog();
+            if (result == false)
+            {
+                treeviewList.Items.Clear();
+            }
+
+            fileName = ofd.FileName;
+            var viewModel = (MainViewModel)DataContext;
+            var hold_days_text = this.Holding_Period.Text;
+            int hold_days = 0;
+            if ( hold_days_text.Length >= 2 && Int32.TryParse(hold_days_text, out hold_days) )
+            {
+                var records = viewModel.LoadCsvData(fileName, hold_days);
+                treeviewList.ItemsSource = records;
+            }
+            else
+            {
+                var records = viewModel.LoadCsvData(fileName, 0);
+                treeviewList.ItemsSource = records;
+            }
+            
+        }
+
+        private void Search_Stcok_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if ( this.Search_Stcok.Text.Length >= 3 )
+            {
+                var viewModel = (MainViewModel)DataContext;
+                var stocks = this.Search_Stcok.Text.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries ).Select(x => x.ToLower());
+                treeviewList.ItemsSource = viewModel.Records.Where(x => stocks.Any( stock => x.StockName.ToLower().Contains(stock))).DistinctBy(x => x.StockName);
+            }
+            if ( this.Search_Stcok.Text.Length == 0 ) 
+            {
+                var viewModel = (MainViewModel)DataContext;                
+                treeviewList.ItemsSource = viewModel.Records;
+            }
+        }
+
+        private void Holding_Period_TextChanged( object sender, TextChangedEventArgs e)
+        {
+            var hold_days_text = this.Holding_Period.Text;
+            int hold_days = 0;
+            if ( hold_days_text.Length >= 2 && Int32.TryParse(hold_days_text, out hold_days ) )
+            {
+                
+            }
+        }
+    }
+}
